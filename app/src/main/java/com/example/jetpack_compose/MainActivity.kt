26e1496e7f273bd797fe72b487e01537dbf436ef
M3NGSZE <mengse.thun13@gmail.com>
2025-08-13 16:10:43 +0700

    preview part 1

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.jetpack_compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpack_compose.ui.theme.JetpackcomposeTheme
 
@@ -29,11 +32,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackcomposeTheme {
-                FirstUi()
+//                FirstUi()
+//                MakeCard()
+                GreetingPreview()
             }
         }
 
 
+    }
+}
+
+@Preview(name = "Light Mode", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun GreetingPreview() {
+    Greeting("Mengse")
+}
+
+@Composable
+fun Greeting(name: String){
+    Text("$name & Chanelle" )
+}
+
+
+@Preview(
+    widthDp = 50,
+    heightDp = 50,
+    showBackground = true
+)
+@Preview(
+    widthDp = 50,
+    heightDp = 60,
+    showBackground = true
+)
+@Composable
+fun MakeCard(){
+    Box(Modifier.background(Color.Yellow)){
+        Text("Hello world")
     }
 }
 
