6ac64788a96e0b40b73a269144956a327b48dab2
M3NGSZE <mengse.thun13@gmail.com>
2025-08-14 22:02:36 +0700

    preview like how many i don't remember

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.jetpack_compose
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.Space
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -32,16 +33,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpack_compose.ui.theme.JetpackcomposeTheme
 
-import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
@@ -63,7 +61,10 @@ class MainActivity : ComponentActivity() {
 //                MakeCard()
 //                GreetingPreview()
 //                CounterPreview()
-                RenderCard()
+//                RenderCard()
+//                ScoreCounter()
+//                RowCount()
+
             }
         }
 
@@ -71,8 +72,52 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
+@Composable
+fun SimpleComposable1( modifier: Modifier = Modifier) { Text("Hello World") }
 
-@Preview(showBackground = true)
+
+//@Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+//@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_MASK)
+@Composable
+fun SimpleComposable( modifier: Modifier = Modifier) { Text("Hello World") }
+
+
+@Composable
+fun RowCount(){
+    Row {
+        ScoreCounter()
+        CounterPreview()
+    }
+}
+
+@Composable
+fun ScoreCounter() {
+    var score by remember { mutableStateOf(0) }
+
+    Column(modifier = Modifier.systemBarsPadding()) {
+        Text(text = "Score: $score")
+        Button(onClick = { score++ }) {
+            Text("Increase")
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun CounterPreview() {
+    var count by remember { mutableStateOf(0) }
+    Column (modifier = Modifier.systemBarsPadding()){
+        Text("Count: $count")
+        Button(onClick = { count++ }) {
+            Text("Increase")
+        }
+    }
+}
+
+
+
+//@Preview(showBackground = true)
 @Composable
 fun InfiniteAnimationPreview() {
     val infiniteTransition = rememberInfiniteTransition(label = "infiniteTransition")
@@ -134,17 +179,7 @@ fun RenderCard(){
 }
 
 
-//@Preview(showBackground = true)
-@Composable
-fun CounterPreview() {
-    var count by remember { mutableStateOf(0) }
-    Column (modifier = Modifier.systemBarsPadding()){
-        Text("Count: $count")
-        Button(onClick = { count++ }) {
-            Text("Increase")
-        }
-    }
-}
+
 
 
 
